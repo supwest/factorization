d31c6bb937fa2245c3200c7445fad4fa6e4c96d1
Cully West <cullywest@gmail.com>
2016-09-23 23:42:36 -0600

    add theano

--- factorization/factorize.py
+++ factorization/factorize.py
@@ -1,8 +1,25 @@
 import numpy as np
 import scipy.sparse as sps
+import theano.tensor a T
 
 
+class Factorizer(object):
+    '''
+    Class for creating a factorization machine recommender
 
+    '''
+
+    def __init__(self, matrix, num_factors=5, loss='rss'):
+        '''
+        Input: A numpy sparse matrix
+        '''
+        self.matrix = matrix
+        self.factors = num_factors
+        self.loss = loss
+
+
+    def factorize(self):
+        pass
 
 
 
@@ -16,3 +33,16 @@ if __name__ == '__main__':
 
     sparse_matrix = sps.coo_matrix((ratings, (users, movies)), shape=(np.unique(users).shape[0]+1, movies.shape[0]+1))
 
+
+    '''
+    from theano import sparse
+    x = sparse.csr_matrix(name='x', dtype='float32')
+    z = sparse.csr_matrix(name='z', dtype='float32')
+    y = sparse.structured_dot(x, z)
+    f = theano.function([x,z], y)
+
+    n = sps.csr_matrix(np.array([[1,0], [0,3]], dtype='float32'))
+    a = f(n, n)
+    n.toarray()
+    a.toarray()
+    '''
