824f725a4abba85aff423de89322dfc1db826be2
Cully West <cullywest@gmail.com>
2016-10-02 13:53:42 -0600

    finished uv decomp

--- factorization/factorize.py
+++ factorization/factorize.py
@@ -18,25 +18,71 @@ class Factorizer(object):
         self.matrix = matrix
         self.factors = num_factors
         self.loss = loss
+        self.U = np.random.rand(self.matrix.shape[0], self.factors)
+        self.V = np.random.rand(self.factors, self.matrix.shape[1])
+        self.non_zeros = self.matrix.nonzero()
+        self.mean_rating = self.matrix[self.non_zeros].mean()
+        self.user_means = self._get_user_means()
+
+    def _get_user_means(self):
+        '''
+        Find the mean of the user ratings
+        Input: None
+        Output: np.array of means
+        '''
+        m = np.zeros(self.matrix.shape[0])
+        for i in range(self.matrix.shape[0]):
+            m[i] = self.matrix[self.matrix.getrow(i).nonzero()].mean()
+        return m
+
+    def factorize(self, num_iter=5, verbose=True):
+        '''
+        Uses GD to find the UV decomposition of the matrix
+        Input: num_iter (int) number of iterations to complete
+               verbose (bool) whether to print iteration and error
+        Output: none
+        '''
+
+        X = T.fmatrix('X')
+        Uu = theano.shared(self.U)
+        Vv = theano.shared(self.V)
+
+        cost = T.sum(T.square(X - T.dot(Uu, Vv))[self.non_zeros])
+
+        du, dv = theano.grad(cost, [Uu, Vv])
+        train = theano.function(inputs = [X], outputs = cost, updates = ((Uu, Uu-0.0001*du), (Vv, Vv-0.0001*dv)))
+
+        print "Training"
+        for i in range(num_iter):
+            train(self.matrix.toarray().astype('float32'))
+            if verbose:
+                print "Iteration {0} of {1}".format(i+1, num_iter)
+                self._print_error(Uu, Vv) 
+        self.U = Uu.get_value()
+        self.V = Vv.get_value()
+        
+
+    def _print_error(self, Uu, Vv):
+        '''
+        Prints the error between the estimate and the real ratings
+        Input: U (np.array), V (np.array)
+        Output: None
+        '''
+        est = np.dot(Uu.get_value(), Vv.get_value())
+        err = self.matrix - est
+        err = err[self.non_zeros]
+        print "Error: {}".format(np.mean(np.square(err)))
 
 
-    def factorize(self):
-        print "factorize"
-        u = np.random.rand(self.matrix.shape[0], self.factors)
-        v = np.random.rand(self.factors, self.matrix.shape[1])
-        print u.shape
-        for i in range(2):
-            v, _, _, _ = np.linalg.lstsq(u, self.matrix.todense())
-            u_temp, _, _, _ = np.linalg.lstsq(v.T, self.matrix.todense().T)
-            u = u_temp.T
-            print np.sum(np.square(np.dot(u, v)-self.matrix))
-        print u
-        print v
-        print u.shape
-        print v.shape
-        return u, v
+class FactorizationMachine(object):
+    '''
+    Class to create a Factorization Machine Recommender
+
+
+    '''
+
+    def __init__(self):
         pass
-
 
 
 
@@ -47,60 +93,10 @@ if __name__ == '__main__':
     movies = mat[:,1]-1
     ratings = mat[:,2].astype(float)
 
-    sparse_matrix = sps.coo_matrix((ratings, (users, movies)), shape=(np.unique(users).shape[0]+1, movies.shape[0]+1)).tocsr()
+    sparse_matrix = sps.coo_matrix((ratings, (users, movies)), shape=(np.unique(users).shape[0], movies.shape[0])).tocsr()
 
     fact = Factorizer(sparse_matrix)
-    #u, v = fact.factorize()
     
-    from theano import sparse
-    x = sparse.csr_matrix(name='x', dtype='float32')
-    z = sparse.csr_matrix(name='z', dtype='float32')
-    y = sparse.structured_dot(x, z)
-    f = theano.function([x,z], y)
-    n = sps.csr_matrix(np.array([[1,0], [0,3]], dtype='float32'))
-    a = f(n, n)
-    n.toarray()
-    a.toarray()
-    k = 4 #number of latent features
-    #u = theano.shared(np.random.rand(n.shape[0], k))
-    #v = theano.shared(np.random.rand(k, n.shape[1])
-
-    #Xv = np.array([[1,0,0], [1,0,0], [0,0,1], [0,0,0]], dtype="float32")
-    Xv = sparse_matrix
-    Xv_dense = sparse_matrix.toarray().astype('float32')
-    Wv = np.random.rand(sparse_matrix.shape[0], 5).astype("float64")
-    Vv = np.random.rand(5,sparse_matrix.shape[1]).astype("float64")
-    X_dense = T.fmatrix('X')
-    X = sparse.csr_matrix(name='X')
-    W = theano.shared(Wv)
-    V = theano.shared(Vv)
-    h = T.dot(W, V)
-
-    diff = sparse.sub(X,h)
-    
-    dense_cost = T.sum(T.square(T.sub(X_dense, T.dot(W, V)))[sparse_matrix.nonzero()])
-    #cost = sparse.sp_sum(sparse.sqr(sparse.csr_from_dense(sparse.sub(X,T.dot(W,V)))),sparse_grad=True)
-    #cost = sparse.sp_sum(sparse.sqr(X-T.dot(W,V)))
-
-    #dw, dv = theano.grad(cost, [W, V])
-    dw, dv = theano.grad(dense_cost, [W, V])
-    #train = theano.function(inputs = [X], outputs = cost, updates = ((W, W-0.0001*dw), (V, V-0.0001*dv)))
-    train = theano.function(inputs = [X_dense], outputs = dense_cost, updates = ((W, W-0.0001*dw), (V, V-0.0001*dv)))
-
-
-    for i in range(20):
-        print i
-        train(Xv_dense)
-        
-        #train(Xv)
-        ww = W.get_value()
-        vv = V.get_value()
-
-        print np.mean(np.square(np.dot(ww,vv) - sparse_matrix)[sparse_matrix.nonzero()])
-    ww = W.get_value()
-    vv = V.get_value()
-    print np.dot(ww, vv)
-        #print np.sum(np.square(np.dot(ww,vv)-sparse_matrix.toarray())[sparse_matrix.nonzero()])
     a = np.array([[1, 0, 0], [2, 0, 1], [0, 1, 0]])
     s = sps.csr_matrix(a)
     u2 = np.random.rand(3,3)
