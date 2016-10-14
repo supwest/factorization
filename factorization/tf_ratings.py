ca5cf6c336af0cdc8a2470cf1c2b7f43463ff82a
Cully West <cullywest@gmail.com>
2016-10-14 00:24:22 -0600

    add tensorflow start

--- factorization/tf_ratings.py
+++ factorization/tf_ratings.py
@@ -4,9 +4,39 @@ import tensorflow as tf
 
 
 
+def inference(data, k):
+
+    with tf.name_scope('interaction'):
+        u = tf.Variable(tf.random_normal([data.shape[0], k]), name='u')
+        v = tf.Variable(tf.random_normal([k, data.shape[1]]), name='v')
+
+        interaction = tf.matmul(u,v)
+
+    return interaction
+
+def loss(pred_mat, labels):
+    
+
+    loss = tf.sparse_reduce_sum(tf.sparse_add(labels, -1.0*pred_mat)**2)
+    return loss
+
+
+def training(loss, learning_rate):
+
+
+    tf.scalar_summary(loss.op.name, loss)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
+
+    global_step = tf.Variable(0, name='global_step', trainable=False)
+    train_op = optmizer.minimize(loss, global_step=global_step)
+    return train_op
+
+def evaluate(pred_mat, labels):
+    return tf.reduce_mean(pred_mat-labels)**2)
+
 
 
 if __name__ == '__main__':
-    mat = np.load('small.npy')
-
+    df = pd.read_csv('books.csv')
+    
     #users = tf.contrib
